174c0e005f8dd4d475e3bc34c74b8de64bca5a3c
ciobanu andrei <cio3banu@gmail.com>
2022-01-27 12:03:26 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $grupa="W-1841";
+    $specialitatea="Administrarea aplicatiilor web";
+    
+    return view('welcome', ['grupa' => $grupa, 'specialitatea'=>$specialitatea]);});
+
+Route::get('/about', function () {return view('about');});
+
+Route::get('/contacts', function () {return view('contacts');});
